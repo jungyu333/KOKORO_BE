dd13e8a79e01d395352067a27527068769c75373
jungyu <96876293+jungyu333@users.noreply.github.com>
2025-06-15 22:10:52 +0900

    [FEAT] Add Global Exception & Standard Response (#5)

--- src/kokoro_be/app/server.py
+++ src/kokoro_be/app/server.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
+from .core.exceptions import BaseCustomException
 from .core.middleware import SQLAlchemyMiddleware
+
+
+def init_listener(app_: FastAPI) -> None:
+    @app_.exception_handler(BaseCustomException)
+    async def custom_exception_handler(request: Request, exc: BaseCustomException):
+        return JSONResponse(
+            status_code=exc.code,
+            content={
+                "code": exc.code,
+                "message": exc.message,
+                "data": exc.data,
+            },
+        )
 
 
 def make_middleware() -> list[Middleware]:
